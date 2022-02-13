f4450382f6d2cec95ee380e5673457fb16f97552
Kaisar Yerdenbekov <sysreqx@gmail.com>
2022-02-13 14:45:40 +0600

    Annotation. PostConstruct, PreDestroy.

--- src/main/java/com/kaisar/springApp1/RhythmAndBluesMusic.java
+++ src/main/java/com/kaisar/springApp1/RhythmAndBluesMusic.java
@@ -1,11 +1,24 @@
 package com.kaisar.springApp1;
 
 import org.springframework.stereotype.Component;
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 
 import java.util.ArrayList;
 
 @Component
 public class RhythmAndBluesMusic implements Music{
+
+    @PostConstruct
+    public void rhythmAndBluesMusicPostConstruct() {
+        System.out.println("Rhythm & blues (R&B) Initialization via @PostConstruct");
+    }
+
+    @PreDestroy
+    public void rhythmAndBluesMusicPreDestroy() {
+        System.out.println("Rhythm & blues (R&B) Initialization via @PreDestroy");
+    }
+
     @Override
     public String getSong() {
         return "Rhythm & blues (R&B) music getSong()";
